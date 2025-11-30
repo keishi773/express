27ff49d11d058d09f4013886eff53d952b4b93e5
unknown <keishi777@hotmail.co.jp>
2025-11-30 09:50:57 +0900

    notes mask

--- routes/notes.js
+++ routes/notes.js
@@ -1,19 +1,19 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 
-// 接続情報を設定
-const { MongoClient } = require("mongodb");
-// マスク
-const uri = "******";
-const client = new MongoClient(uri);
+// // 接続情報を設定
+// const { MongoClient } = require("mongodb");
+// // マスク
+// const uri = "******";
+// const client = new MongoClient(uri);
 
-router.get('/', async (req, res) => {
-// データベース、コレクションを指定
-const database = client.db('notes');
-const notes = database.collection('notes');
-// idが１のドキュメントを取得
-const query = { id: 2 };
-const note = await notes.findOne(query);
-res.json(note);
-})
-module.exports = router;
+// router.get('/', async (req, res) => {
+// // データベース、コレクションを指定
+// const database = client.db('notes');
+// const notes = database.collection('notes');
+// // idが１のドキュメントを取得
+// const query = { id: 2 };
+// const note = await notes.findOne(query);
+// res.json(note);
+// })
+// module.exports = router;
